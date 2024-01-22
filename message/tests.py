9fc963c5b2f9c9eed056f809e2ee1ddeec1bc8e3
ali <alilaser8080@gmail.com>
2024-01-22 19:18:32 +0330

    Add version6: django_06_test-case_complete

--- message/tests.py
+++ message/tests.py
@@ -1,7 +1,20 @@
 from django.test import SimpleTestCase
+from django.urls import reverse
 
 
 class MessagePageTest(SimpleTestCase):
     def test_exist_url_at_correct_location(self):
         response = self.client.get('/message/')
         self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse('message'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name(self):
+        response = self.client.get(reverse('message'))
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_template_content(self):
+        response = self.client.get('/message/')
+        self.assertContains(response, "<h1>Hello Django!</h1>")
